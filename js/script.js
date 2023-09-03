693024330354cee93f9f8481df2b7e915067cadd
xstylizt <x.stylizt@gmail.com>
2023-09-03 23:45:38 +0700

    Update mergeCells function more dynamically

--- js/script.js
+++ js/script.js
@@ -9,54 +9,135 @@ $(document).ready(function(){
         i = CM.length,
         j = Data.length;
 
-
+        
         // Automatically merge columns based on row values for content
         for (var r = 0; r < Data.length; r++) {
                         var rc = 1,
                             c = CM.length;
 
                         while (c--) {
-                            var cd = Data[r][CM[c].dataIndx],
-                                cd_prev = Data[r][CM[c - 1] ? CM[c - 1].dataIndx : undefined];
+                            var cd = Data[r][CM[c].dataIndx]
+                            var cd_prev = Data[r][CM[c - 1] ? CM[c - 1].dataIndx : undefined];
                             
                             if (cd_prev !== undefined && cd == cd_prev) {
                                 rc++;
                             } else if (rc > 1) {
-                                mc.push({ r1: r, c1: c, rc: 1, cc: rc });
+                                mc.push({cellValue: cd, r1: r, c1: c, rc: 1, cc: rc });
                                 rc = 1;
                             }
                         }
-                    }
+        }
 
-
-                    while (i--) {
-                        var dataIndx = CM[i].dataIndx,
-                            rc = 1;
+        while (i--) {
+            var dataIndx = CM[i].dataIndx,
+                rc = 1;
+            
+            while (j--) {   
+                var cd = Data[j][dataIndx],
+                cd_prev = Data[j - 1] ? Data[j - 1][dataIndx] : undefined;
                         
-                        while (j--) {   
-                            var cd = Data[j][dataIndx],
-                            cd_prev = Data[j - 1] ? Data[j - 1][dataIndx] : undefined;
-                                    
-                            if (cd_prev !== undefined && cd == cd_prev) {
-                                rc++;
-                            }
-                            else if (rc > 1) {
-                                    mc.push({ r1: j, c1: i, rc: rc, cc: 1 });
-                                rc = 1;
-                                }
-                            }
-                            
-                            j = Data.length; // Reset j for the next column
-                        }
+                if (cd_prev !== undefined && cd == cd_prev) {
+                    rc++;
+                }
+                else if (rc > 1) {
+                        mc.push({cellValue: cd, r1: j, c1: i, rc: rc, cc: 1 });
+                    rc = 1;
+                    }
+                }
+                j = Data.length; // Reset j for the next column
+            }
+    
  
+            
+    let heading_mc = mc.    sort((a, b) => {
+        if (a.r1 !== b.r1) {
+            return a.r1 - b.r1;
+        } else if (a.c1 !== b.c1) {
+            return a.c1 - b.c1;
+        } else if (a.rc !== b.rc) {
+            return a.rc - b.rc;
+        } else {
+            return a.cc - b.cc;
+        }
+    });
 
-        let heading_mc = mc
+    const groupedData = {};
+    const sortedData = [];
 
+    heading_mc.forEach(item => {
+    const key = `${item.c1}-${item.rc}-${item.cc}`;
+    if (!groupedData[key]) {
+        groupedData[key] = [];
+    }
+    groupedData[key].push(item);
+    });
 
-             
+    Object.values(groupedData).forEach(group => {
+    sortedData.push(...group);
+    });
+    
+
+    function mergeConsecutiveObjects(arr) {
+        const result = [];
+        let currentObj = { ...arr[0] };
+      
+        for (let i = 1; i < arr.length; i++) {
+          const nextObj = arr[i];
+      
+          if (
+            currentObj.c1 === nextObj.c1 &&
+            currentObj.cc === nextObj.cc &&
+            currentObj.rc + currentObj.r1 === nextObj.r1 &&
+            currentObj.cellValue === nextObj.cellValue
+          ) {
+            currentObj.rc += nextObj.rc;
+          } else {
+            result.push(currentObj);
+            currentObj = { ...nextObj };
+          }
+        }
+      
+        result.push(currentObj);
+      
+        return result;
+      }
+      
+      const sortedData2 = mergeConsecutiveObjects(sortedData)
+
+      const basedArray = sortedData2.filter(item => item.cc !== 1);
+
+        const componentArray = sortedData2.filter(itemA =>
+        !basedArray.some(itemB =>
+            Object.keys(itemA).every(key => itemA[key] === itemB[key])
+        )
+        );
+
+        console.log(basedArray)
+        console.log(componentArray)
+
+        var temp = []
+        for(var i=0; i < basedArray.length ; i++){
+            for(var j=0; j < componentArray.length; j++){
+                if(basedArray[i].r1 == componentArray[j].r1 &&
+                basedArray[i].c1 == componentArray[j].c1 &&
+                basedArray[i].rc == componentArray[j].rc && 
+                basedArray[i].cellValue === componentArray[j].cellValue
+                ){
+                    for(var k=0; k < basedArray[i].cc ; k++){
+                        temp.push({cellValue: basedArray[i].cellValue,r1: basedArray[i].r1, c1: basedArray[i].c1 + k, rc: basedArray[i].rc, cc: 1})
+                    }
+                }
+            }
+        }
+
+        const real_componentArray = componentArray.filter(itemA =>
+            !temp.some(itemB => Object.keys(itemA).every(key => itemA[key] == itemB[key]))
+        )
+
+        const real_mc = basedArray.concat(real_componentArray)
 
     const r_mc = heading_mc
-    grid.option("mergeCells", r_mc);
+    grid.option("mergeCells", real_mc);
     if (refresh) {
         grid.refreshView();
     }
@@ -64,7 +145,7 @@ $(document).ready(function(){
 
     var data = [
         ["Employee information", "Employee information", "Employee information", "Section", "Role", "Skill", "Skill", "Skill", "Skill", "Skill", "Skill", "Skill", "Skill"],
-        ["Id", "First_name", "Last_name", "Section", "Role", "Frontend framework", "Frontend framework", "Frontend framework", "Frontend framework", "Backend framework", "Backend framework", "Backend framework", "Backend framework"],
+        ["Employee information", "Employee information", "Employee information", "Section", "Role", "Frontend framework", "Frontend framework", "Frontend framework", "Frontend framework", "Backend framework", "Backend framework", "Backend framework", "Backend framework"],
         ["Id", "First_name", "Last_name", "Section", "Role", "React", "Angular", "Vue", "Svelte", "Express.js", "Laravel", "Springboot", "Django"],
         [1, "Sakura", "Tanaka", "Software developer", "Frontend developer", "✔", "✔", "", "", "", "", "", ""],
         [2, "Ravi", "Patel", "Software developer", "Backend developer", "", "", "", "✔", "", "", "✔", ""],
@@ -96,7 +177,7 @@ $(document).ready(function(){
         [28, "Hiroshi", "Suzuki", "Software developer", "Frontend developer", "✔", "✔", "", "✔", "", "", "", "✔"],
         [29, "Ananya", "Gupta", "Software developer", "Backend developer", "", "", "", "✔", "✔", "", "✔", ""],
         [30, "Raj", "Singh", "Software developer", "Full Stack developer", "✔", "", "✔", "", "", "✔", "", "✔"],
-        [31, "Sakura", "Tanaka", "Software developer", "Frontend developer", "✔", "✔", "", "", "", "", "", ""],
+        [31, "Sakuraasdasdasdasdas", "Tanaka", "Software developer", "Frontend developer", "✔", "✔", "", "", "", "", "", ""],
         [32, "Ravi", "Patel", "Software developer", "Backend developer", "", "", "", "✔", "", "", "✔", ""],
         [33, "Ji-eun", "Kim", "Software developer", "Backend developer", "", "", "", "", "✔", "", "", ""],
         [34, "Chen", "Liu", "Software developer", "Full Stack developer", "✔", "", "", "", "", "✔", "", ""],
@@ -119,16 +200,16 @@ $(document).ready(function(){
     ]
 
     n_row = 3
-    var rPP = 20;
+    var rPP = 15;
     var obj = {     
-        minWidth: 800,
+        minWidth: 300,
         height:  rPP <= 12 ? 'flex': 600,
         editable: false,
         sortModel: false,
         freezeRows: 3,
         numberCell: {show: false},
         scrollModel: { autoFit: true },
-        flex: {one: false},
+        flex: {one: true},
         dataModel: { data: data },
         columnTemplate: {
             align: 'center', valign: 'center'
@@ -137,11 +218,13 @@ $(document).ready(function(){
     }
 
     
-
-   
     $('.btn-show').on('click',function(ev){
         $('.hidden').toggle()   
-        var grid = pq.grid("#automerged-modified-table", obj)
+        var grid = pq.grid("#automerged-modified-table", obj).on("refresh refreshCell", function (evt, ui) {
+            if (ui.source != 'flex') {
+                this.flex();
+            }
+        });
         mergeCells(grid, data, true)
     })
 
